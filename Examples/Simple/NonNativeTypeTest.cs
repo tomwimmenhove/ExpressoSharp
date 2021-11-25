eab85d91e1c28ad2fb58ba4ae3bddc3ad43411d4
Tom Wimmenhove <tom.wimmenhove@gmail.com>
2021-11-25 21:23:07 +0000

    Added MIT license

--- Examples/Simple/NonNativeTypeTest.cs
+++ Examples/Simple/NonNativeTypeTest.cs
@@ -1,3 +1,9 @@
+/* This file is part of Expresso
+ *
+ * Copyright (c) 2021 Tom Wimmenhove. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ */
+
 namespace Simple
 {
     public class NonNativeTypeTest
--- Expresso/ExpressoParameter.cs
+++ Expresso/ExpressoParameter.cs
@@ -1,3 +1,9 @@
+/* This file is part of Expresso
+ *
+ * Copyright (c) 2021 Tom Wimmenhove. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for details.
+ */
+
 using System;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
